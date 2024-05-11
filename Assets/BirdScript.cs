0c83ef3898d88d09389a0b79b6a1d5182bf05e41
Yshinprograms <82699007+Yshinprograms@users.noreply.github.com>
2024-05-12 00:25:00 +0800

    Add Game Over Screen
    Add Game Over UI with UnityEngine.SceneManagement
    Add restart button
    Add game over conditions

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BirdScript : MonoBehaviour
@@ -8,20 +9,34 @@ public class BirdScript : MonoBehaviour
     public Rigidbody2D myRigidbody;
     public float flapStrength;
     public int test;
+    public LogicScript logic;
 
     void Start() //runs precisely once
     {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
         gameObject.name = "Lil Birdy";
     }
 
 
     void Update() //runs constantly while script is enabled
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && logic.birdIsAlive)
         {
             //Vector2 is a shortcut that gives the vector (0,1), which is 1 unit in the up direction
             //We multiply by 10 to give the flap more power
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
+
+        if (transform.position.y <= -21)
+        {
+            logic.gameOver();
+        }
+        //We use onCollision instead of onTrigger because the pipes now are solid objects
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 //UI is another game object, but we need to import the UI class
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class LogicScript : MonoBehaviour
     //Adds a variable slot to the inspector menu
     //We can drag and drop UI text into the slot
     //then it fills up the scoreText variable in the script
+    public GameObject gameOverScreen;
+    public bool birdIsAlive = true;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
@@ -24,5 +27,26 @@ public class LogicScript : MonoBehaviour
         //Then assign to scoreText
         //Needs scoreText.text because Text is a class in Unity
         //of which .text is only one of the properties
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        /* 1. SceneManager.GetActiveScene() gets a reference to the scene currently loaded and running
+         * 2. SceneManager.GetActiveScene().name extracts the name of the active scene
+         * 3. SceneManager.LoadScene loads scene that matches name
+         * which in this case is the currently running scene, basically resetting level
+         */
+
+    }
+
+    public void gameOver()
+    {
+        birdIsAlive = false;
+        gameOverScreen.SetActive(true);
+        /* Triggers when bird crashes into pipe
+         * we call this in BirdScript
+         * 
+         */
     }
 }
